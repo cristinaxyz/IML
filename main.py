afd99bfea5d1b552487057bc195e833e757e4e9a
cristinaxyz <c.rau.1@student.rug.nl>
2026-01-23 00:06:22 +0200

    fixed num_classes to match with data and attempt to compare loss

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from processing import PreprocessingData
 from model import CNN
 from training import train_one_epoch, validate_one_epoch
+import torch 
 import torch.optim as optim
 import torch.nn as nn
 
@@ -9,11 +10,13 @@ def main():
     datamodel = PreprocessingData(3404, transform=True)
     train_loader, val_loader, test_loader = datamodel.split_data()
 
-    model = CNN(num_classes=10)
+    model = CNN(num_classes=11)
 
     criteria = nn.CrossEntropyLoss()
 
-    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9, weight_decay=5e-4)
+    learning_rate = 0.001
+
+    optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=0.9, weight_decay=5e-4)
 
     epochs = 110
     best_value_loss=float('inf')
@@ -23,7 +26,18 @@ def main():
 
     for epoch in range(epochs):
         train_loss, train_acc = train_one_epoch(model, train_loader, optimizer, criteria)
-        validation_loss, validation_acc = validate_one_epoch(model, val_loader, optimizer, criteria)
+        validation_loss, validation_acc = validate_one_epoch(model, val_loader, criteria)
+
+        if validation_loss < best_value_loss:
+            best_value_loss = validation_loss
+            waiting_index = 0
+            torch.save(model.state_dict(), "best.pth")
+        else:
+            waiting_index = waiting_index + 1
+            if waiting_index == epochs_not_improving:
+                learning_rate = learning_rate * 0.5
+
+        print(train_acc, validation_acc)
 
         #change learning rate
     
